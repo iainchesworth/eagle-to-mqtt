094faa9892c66bbb2debdb384fcd8a24788f52d9
Iain Chesworth <iain.chesworth@gmail.com>
2021-07-21 22:40:12 +1000

    Add support for gcov code coverage (integrated into the build system)

--- src/notifications/common/metering_payload_types.cpp
+++ src/notifications/common/metering_payload_types.cpp
@@ -1,1 +1,25 @@
 #include "notifications/common/metering_payload_types.h"
+
+template<>
+streamable_any::streamable_any(uint8_t&& t) : 
+	std::any(uint8_t(t)), 
+	streamer([](std::ostream& os, streamable_any const& self)
+	{
+		// In above template, the uint8_t type gets cast to a char (e.g. 100 = d, 90 = Z, and so forth)...don't do that.
+		os << std::to_string(std::any_cast<uint8_t>(self));
+	})
+{
+}
+
+template<>
+streamable_any::streamable_any(std::chrono::time_point<std::chrono::system_clock>&& t) : 
+	std::any(std::chrono::time_point<std::chrono::system_clock>(t)), 
+	streamer([](std::ostream& os, streamable_any const& self)
+	{
+		using namespace date; // Leverage Howard Hinnant's date library to get the iostream support.
+
+		// <sigh> why didn't the C++17 standard allow iostreams support for <chrono>
+		os << std::any_cast<std::chrono::time_point<std::chrono::system_clock>>(self);
+	})
+{
+}
--- src/notifications/common/metering_payload_types.h
+++ src/notifications/common/metering_payload_types.h
@@ -21,6 +21,7 @@ struct streamable_any : public std::any
 		a.streamer(os, a);
 		return os;
 	}
+
 	template<class T, std::enable_if_t<!std::is_same<std::decay_t<T>, streamable_any>{}, bool> = true>
 	streamable_any(T&& t) : std::any(std::forward<T>(t)), streamer([](std::ostream& os, streamable_any const& self)
 		{
@@ -28,25 +29,12 @@ struct streamable_any : public std::any
 		})
 	{
 	}
-	template<>
-	explicit streamable_any(uint8_t&& t) : std::any(uint8_t(t)), streamer([](std::ostream& os, streamable_any const& self)
-		{
-			// In above template, the uint8_t type gets cast to a char (e.g. 100 = d, 90 = Z, and so forth)...don't do that.
-			os << std::to_string(std::any_cast<uint8_t>(self));
-		})
-	{
-	}
-	template<>
-	explicit streamable_any(std::chrono::time_point<std::chrono::system_clock>&& t) : std::any(std::chrono::time_point<std::chrono::system_clock>(t)), streamer([](std::ostream& os, streamable_any const& self)
-		{
-			using namespace date; // Leverage Howard Hinnant's date library to get the iostream support.
-
-			// <sigh> why didn't the C++17 standard allow iostreams support for <chrono>
-			os << std::any_cast<std::chrono::time_point<std::chrono::system_clock>>(self);
-		})
-	{
-	}
 };
+
+template<>
+streamable_any::streamable_any(uint8_t&& t);
+template<>
+streamable_any::streamable_any(std::chrono::time_point<std::chrono::system_clock>&& t);
 
 using MeteringPayload_DeviceId = EthernetMacId;
 using MeteringPayload_ElementMap = std::unordered_map<std::string, streamable_any>;
